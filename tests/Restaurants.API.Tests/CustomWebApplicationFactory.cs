526fb84a51070ae71564cf915cc89d78851e988f
TamLe99 <tamlv@harmony-at.vn>
2025-01-21 15:59:16 +0700

    fixed unitest api

---
+++ tests/Restaurants.API.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.API.Tests;
+
+public class CustomWebApplicationFactory<TEntryPoint> : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Development");
+        builder.ConfigureTestServices(services =>
+        {
+            // Mock các service nếu cần
+            services.Replace(ServiceDescriptor.Scoped<IRestaurantsRepository>(_ => Mock.Of<IRestaurantsRepository>()));
+        });
+    }
+}
